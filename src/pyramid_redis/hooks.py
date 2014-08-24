72e3988999936ffa5406d47984a97c1d1d7a4d00
Ron Drongowski <ron.drongowski@gmail.com>
2014-08-24 13:38:33 +0200

    Make a unix_socket_path configurable

--- src/pyramid_redis/hooks.py
+++ src/pyramid_redis/hooks.py
@@ -2,10 +2,10 @@
 
 """Provides a ``RedisFactory`` to get a configured redis client from a
   settings dictionary, e.g.::
-  
+
       >>> factory = RedisFactory()
       >>> client = factory({'redis.url': 'redis://localhost:6379'})
-  
+
   And ``GetRedisClient`` which wraps the factory so it can be used as a
   Pyramid request method.
 """
@@ -17,7 +17,7 @@ __all__ = [
 
 import logging
 logger = logging.getLogger(__name__)
-
+import pyramid.exceptions
 import redis
 
 try:
@@ -29,82 +29,100 @@ from zope.component import getGlobalSiteManager
 from zope.interface import Interface
 from zope.interface import directlyProvides
 
-class IRedisConnectionPool(Interface):
-    """Marker interface provided by RedisConnectionPool utilities."""
+
+class IRedisClientConfiguration(Interface):
+    """Marker interface provided by RedisConnectionPool utilities or
+    unix_socket_path"""
 
 
 class ParseConfig(object):
     """Parse the application settings into connection pool kwargs."""
-    
+
     def __init__(self, **kwargs):
         self.parse_url = kwargs.get('parse_url', urlparse.urlparse)
-    
+
     def __call__(self, settings):
         """Unpack the settings. Parse the url into components and build
-          a dict to return.
+          a dict to return. As an alternative, you may also provide a
+          unix_socket_path.
         """
-        
-        # Unpack.
-        url = settings['redis.url']
         db = settings.get('redis.db', 0)
-        max_connections = settings.get('redis.max_connections', None)
-        
-        # Parse into a config dict.
-        o = self.parse_url(url)
-        config = {
-            'host': o.hostname,
-            'port': o.port,
-            'db': int(db)
-        }
-        if o.password:
-            config['password'] = o.password
-        if max_connections is not None:
-            config['max_connections'] = int(max_connections)
+        config = {'db': int(db)}
+
+        if 'redis.url' in settings.keys():
+            # Unpack.
+            url = settings['redis.url']
+
+            # Parse into a config dict.
+            o = self.parse_url(url)
+            config.update({
+                'host': o.hostname,
+                'port': o.port,
+            })
+            if o.password:
+                config['password'] = o.password
+
+            max_connections = settings.get('redis.max_connections', None)
+            if max_connections is not None:
+                config['max_connections'] = int(max_connections)
+        elif 'redis.unix_socket_path' in settings.keys():
+            config['unix_socket_path'] = settings['redis.unix_socket_path']
+        else:
+            raise pyramid.exceptions.ConfigurationError(
+                """To use redis with pyramid, redis.url or
+                redis.unix_socket_path should be provided"""
+            )
         return config
-    
+
 
 class RedisFactory(object):
+
     def __init__(self, **kwargs):
         self.get_registry = kwargs.get('get_registry', getGlobalSiteManager)
         self.parse_config = kwargs.get('parse_config', ParseConfig())
         self.pool_cls = kwargs.get('pool_cls', redis.BlockingConnectionPool)
         self.provides = kwargs.get('provides', directlyProvides)
         self.redis_cls = kwargs.get('redis_cls', redis.StrictRedis)
-    
+
     def __call__(self, settings, registry=None):
-        """Returns a ``redis`` client that uses a connection pool registered in
+        """Returns a ``redis`` client that uses a client configuration registered in
           the ``registry`` provided that is, in turn, configured with the
           ``settings`` provided.
         """
-        
+
         # If called without a registry, i.e.: not within the context of a
         # Pyramid application, then register the connection pool in a
         # zope.component registry.
         if registry is None:
             registry = self.get_registry()
-        
-        # Query the registry for a connection pool. If it doesn't exist,
+
+        # Query the registry for a client_configuration. If it doesn't exist,
         # instantiate and register one for next time.
-        connection_pool = registry.queryUtility(IRedisConnectionPool)
-        if not connection_pool:
-            kwargs = self.parse_config(settings)
-            connection_pool = self.pool_cls(**kwargs)
-            self.provides(connection_pool, IRedisConnectionPool)
-            registry.registerUtility(connection_pool, IRedisConnectionPool)
-        
+        redis_client_conf = registry.queryUtility(IRedisClientConfiguration)
+        if not redis_client_conf:
+            redis_client_conf = self._create_clientconfig()
+            self.provides(redis_client_conf, IRedisClientConfiguration)
+            registry.registerUtility(redis_client_conf,
+                                     IRedisClientConfiguration)
+
         # And use it to instantiate a redis client.
-        return self.redis_cls(connection_pool=connection_pool)
-    
+        return self.redis_cls(**redis_client_conf)
+
+    def _create_client_config(self, settings):
+        kwargs = self.parse_config(settings)
+        if 'host' in kwargs.keys():
+            return {'connection_pool': self.pool_cls(**kwargs)}
+        else:
+            return kwargs
 
 
 class GetRedisClient(object):
+
     """Provide the redis factory as a Pyramid request method."""
-    
+
     def __init__(self, **kwargs):
         self.redis_factory = kwargs.get('redis_factory', RedisFactory())
-    
+
     def __call__(self, request):
         registry = request.registry
         return self.redis_factory(registry.settings, registry=registry)
-    
-
